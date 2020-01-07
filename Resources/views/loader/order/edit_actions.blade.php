b4db8abc96b75064fac4cd18c14b4936b58652d7
Your Name <you@example.com>
2020-01-07 09:25:34 -0300

    .

--- Resources/views/loader/order/edit_actions.blade.php
+++ Resources/views/loader/order/edit_actions.blade.php
@@ -1,2 +1,4 @@
+@if($order->order_client)
 <a href="" class="btn btn-outline-info mr-3" data-toggle="modal" data-target="#modal_send_email"><i class="fa fa-envelope-o"></i> Email</a>
 @include('emailgmail::loader.order.modals.modal_send_email')
+@endif
